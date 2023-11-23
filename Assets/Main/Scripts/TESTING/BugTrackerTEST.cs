98796193cf962066baf7ff647651bfaee42c3df0
GeorQ <georqv2@icloud.com>
2023-11-23 19:09:49 +0500

    bug tracker test

---
+++ Assets/Main/Scripts/TESTING/BugTrackerTEST.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BugTrackerTEST : MonoBehaviour
+{
+    private Rigidbody errorRef;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            errorRef.AddForce(Vector3.up);
+        }
+    }
+}
